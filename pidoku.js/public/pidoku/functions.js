d5d941985339ae02b2a0b1682de7a36d752be2f4
César Cordero <rockcesar@linux-debian>
2021-06-30 06:56:11 -0400

    Upgrading

--- pidoku.js/public/pidoku/functions.js
+++ pidoku.js/public/pidoku/functions.js
@@ -46,7 +46,7 @@ $( document ).ready(function() {
               // An explanation of the payment - will be shown to the user:
               memo: "Donation to Sudoku", // e.g: "Digital kitten #1234",
               // An arbitrary developer-provided metadata object - for your own usage:
-              metadata: { /* ... */ }, // e.g: { kittenId: 1234 }
+              metadata: { paymentType: "donation" /* ... */ }, // e.g: { kittenId: 1234 }
             }, {
                   // Callbacks you need to implement - read more about those in the detailed docs linked below:
                   onReadyForServerApproval: function(paymentId) { $("#button_click").prop( "disabled", false ); /* ... */ },
