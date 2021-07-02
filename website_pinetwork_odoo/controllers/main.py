d5c2881ebc030443cc8f3432567219db62374c50
César Cordero <rockcesar@linux-debian>
2021-07-02 15:50:11 -0400

    upgrade

--- website_pinetwork_odoo/controllers/main.py
+++ website_pinetwork_odoo/controllers/main.py
@@ -1,10 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from odoo import http
-from odoo.addons.website.controllers.main import Website
+from odoo.http import request
+import json
 
-class Website(Website):
-    @http.route('/', type='http', auth="public", website=True)
+import requests
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class PiNetworkController(http.Controller):
+    @http.route('/pinetwork', type='http', auth="public", website=True)
     def index(self, **kw):
-        super(Website, self).index(**kw)
         return http.request.render('website_pinetwork_odoo.pinetwork', {})
+        
+    @http.route('/pi-api', type='http', auth="public", website=True, csrf=False, methods=['POST'])
+    def pi_api(self, **kw):
+        
+        if kw['action'] == "approve":
+            url = 'https://api.minepi.com/v2/payments/' + kw['paymentId'] + '/approve'
+            data = {}
+        elif kw['action'] == "complete":
+            url = 'https://api.minepi.com/v2/payments/' + kw['paymentId'] + '/complete'
+            data = {'txid': kw['txid']}
+        
+        re = requests.post(url,json=data,headers={'Authorization':'Key <your Server API Key>'})
+        
+        try:
+            result = re.json()
+        except Exception:
+            result = {"error": "SERVER MESSAGE: " + str(re)}
+        
+        return json.dumps(result)
