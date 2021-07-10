dbae98bc65e5ca41b0d90c16c45c3430b54c5623
César Cordero <rockcesar@linux-debian>
2021-07-10 14:31:11 -0400

    Upgrade

--- index.php
+++ index.php
@@ -170,7 +170,7 @@
             <img src="https://latin-chain.com/latin-chain-logo.jpeg" alt="LatinChain" class="img-fluid" style="width: 100px; height: 100px;" /><br/><br/>
             <a href="https://latin-chain.com/weblatin" class="btn btn-link">MAIN PAGE OF LATINCHAIN</a><br/><br/>
             <a href="https://dev-rockcesar.blogspot.com/2021/05/pi-apps-published.html" class="btn btn-link" >MORE INFO</a><br/><br/>-->
-            
+            <br/><br/>
             <div id="social_div" style="visibility: hidden">Share in Social Networks:<br/><br/>
             <button id="button1" class="btn btn-primary">Share</button><br/><br/>
             </div>
