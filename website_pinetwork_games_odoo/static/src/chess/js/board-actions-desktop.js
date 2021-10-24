a0df288e5338291f8e781f7443f2a94160997fee
ccordero <cesar.cordero.r@gmail.com>
2021-10-24 16:15:16 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/chess/js/board-actions-desktop.js
+++ website_pinetwork_games_odoo/static/src/chess/js/board-actions-desktop.js
@@ -103,7 +103,7 @@ function setDesktopBoard(position = false, sparePieces = false) {
   var onSnapEnd = function() {
     board.position(game.fen());
     gameHistoryAddMove(game.fen());
-    if (game.history().length > 0) $('#btn-take-back').removeClass('hidden');
+    //if (game.history().length > 0) $('#btn-take-back').removeClass('hidden');
   };
 
   board = ChessBoard('board', {
