c7d253c2e65c56969bdee1a23ec55108d050eb84
ccordero <cesar.cordero.r@gmail.com>
2021-11-18 18:55:18 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions.js
+++ website_pinetwork_games_odoo/static/src/js/functions.js
@@ -4,123 +4,123 @@ var accessToken = "";
 var passkey = "";
 const Pi = window.Pi;
 
-function set_points_v2(points) {
-    if(pi_user_id != "" && pi_user_code != "")
-    {
-        var data = {
-            'pi_user_id': pi_user_id,
-            'pi_user_code': pi_user_code,
-            'points': points,
-            'passkey': passkey,
-            'accessToken': accessToken,
-            'csrf_token': odoo.csrf_token,
-        };
-        $.ajaxSetup({async: false});
-        return $.post( "/pi-points", data).done(function(data) {
-            data = JSON.parse(data);
-            if(data.result && points > 0)
-                alert("You won " + points + " points");
-        }).fail(function() {
-            
-        });
-    }
-}
-
-function set_points(points) {
-    try {
-        // Identify the user with their username / unique network-wide ID, and get permission to request payments from them.
-        const scopes = ['username', 'payments'];
-        function onIncompletePaymentFound(payment) {
-            
+$( document ).ready(function() {
+    Pi.init({ version: "2.0", sandbox: $("#sandbox").val() });
+    
+    function set_points_v2(points) {
+        if(pi_user_id != "" && pi_user_code != "")
+        {
             var data = {
-                    'action': 'complete',
-                    'paymentId': payment.identifier,
-                    'txid': payment.transaction.txid,
-                    'app_client': 'auth_example',
-                    'csrf_token': odoo.csrf_token,
-                    'accessToken': accessToken,
-                    'pi_user_code': pi_user_code,
-                    'pi_user_id': pi_user_id,
-                };
-            return $.post( "/pi-api", data).done(function(data) {
-                $("#button_click").prop( "disabled", false );
-                try {
-                    data = JSON.parse(data);
-                    if(data.result && data.completed)
-                    {
-                        alert("A payment was registered. Reload the page to view the changes.");
+                'pi_user_id': pi_user_id,
+                'pi_user_code': pi_user_code,
+                'points': points,
+                'passkey': passkey,
+                'accessToken': accessToken,
+                'csrf_token': odoo.csrf_token,
+            };
+            $.ajaxSetup({async: false});
+            return $.post( "/pi-points", data).done(function(data) {
+                data = JSON.parse(data);
+                if(data.result && points > 0)
+                    alert("You won " + points + " points");
+            }).fail(function() {
+                
+            });
+        }
+    }
+
+    function set_points(points) {
+        try {
+            // Identify the user with their username / unique network-wide ID, and get permission to request payments from them.
+            const scopes = ['username', 'payments'];
+            function onIncompletePaymentFound(payment) {
+                
+                var data = {
+                        'action': 'complete',
+                        'paymentId': payment.identifier,
+                        'txid': payment.transaction.txid,
+                        'app_client': 'auth_example',
+                        'csrf_token': odoo.csrf_token,
+                        'accessToken': accessToken,
+                        'pi_user_code': pi_user_code,
+                        'pi_user_id': pi_user_id,
+                    };
+                return $.post( "/pi-api", data).done(function(data) {
+                    $("#button_click").prop( "disabled", false );
+                    try {
+                        data = JSON.parse(data);
+                        if(data.result && data.completed)
+                        {
+                            alert("A payment was registered. Reload the page to view the changes.");
+                        }
+                    } catch (e) {
                     }
-                } catch (e) {
+                }).fail(function() {
+                    $("#button_click").prop( "disabled", false );
+                });
+            }; // Read more about this in the SDK reference
+
+            Pi.authenticate(scopes, onIncompletePaymentFound).then(function(auth) {
+                pi_user_id = auth.user.uid;
+                pi_user_code = auth.user.username;
+                accessToken = auth.accessToken;
+              
+                //get_user(false);
+                set_points_v2(points);
+                get_user(false);
+            
+              $( "#button_click" ).click(function() {
+                    if(parseFloat($("#pi_donate").val()) > 0)
+                    {
+                        $("#button_click").prop( "disabled", true );
+                        /*setTimeout(function ()
+                        {
+                            $("#button_click").prop( "disabled", false );
+                        }, 10000);*/
+                        transfer();
+                    }
+                    //alert("Click");
+                });
+                //alert('Hello ' + auth.user.username);
+            }).catch(function(error) {
+              console.error(error);
+            });
+        } catch (err) {
+            alert(err);
+            // Not able to fetch the user
+        }
+    }
+
+    function get_user(donation) {
+        if(pi_user_id != "" && pi_user_code != "")
+        {
+            var data = {
+                        'pi_user_id': pi_user_id,
+                        'pi_user_code': pi_user_code,
+                        'accessToken': accessToken,
+                        'csrf_token': odoo.csrf_token,
+                    };
+            $.ajaxSetup({async: false});
+            return $.post( "/get-user", data).done(function(data) {
+                data = JSON.parse(data);
+                if(data.result)
+                {
+                    passkey=data.passkey;
+                    if(data.unblocked)
+                    {
+                        if(donation)
+                            alert("Thank you for your donation. User " + pi_user_code + " unblocked.");
+                    }
                 }
             }).fail(function() {
-                $("#button_click").prop( "disabled", false );
+                
             });
-        }; // Read more about this in the SDK reference
-
-        Pi.authenticate(scopes, onIncompletePaymentFound).then(function(auth) {
-            pi_user_id = auth.user.uid;
-            pi_user_code = auth.user.username;
-            accessToken = auth.accessToken;
-          
-            //get_user(false);
-            set_points_v2(points);
-            get_user(false);
-        
-          $( "#button_click" ).click(function() {
-                if(parseFloat($("#pi_donate").val()) > 0)
-                {
-                    $("#button_click").prop( "disabled", true );
-                    /*setTimeout(function ()
-                    {
-                        $("#button_click").prop( "disabled", false );
-                    }, 10000);*/
-                    transfer();
-                }
-                //alert("Click");
-            });
-            //alert('Hello ' + auth.user.username);
-        }).catch(function(error) {
-          console.error(error);
-        });
-    } catch (err) {
-        alert(err);
-        // Not able to fetch the user
+        }
     }
-}
-
-function get_user(donation) {
-    if(pi_user_id != "" && pi_user_code != "")
-    {
-        var data = {
-                    'pi_user_id': pi_user_id,
-                    'pi_user_code': pi_user_code,
-                    'accessToken': accessToken,
-                    'csrf_token': odoo.csrf_token,
-                };
-        $.ajaxSetup({async: false});
-        return $.post( "/get-user", data).done(function(data) {
-            data = JSON.parse(data);
-            if(data.result)
-            {
-                passkey=data.passkey;
-                if(data.unblocked)
-                {
-                    if(donation)
-                        alert("Thank you for your donation. User " + pi_user_code + " unblocked.");
-                }
-            }
-        }).fail(function() {
-            
-        });
-    }
-}
-
-$( document ).ready(function() {
+    
     $(document).ajaxStop(function() {
                 $("#loading_word").hide();
             });
-    
-    Pi.init({ version: "2.0", sandbox: $("#sandbox").val() });
 
     //alert(PiNetworkClient);
 
--- website_pinetwork_games_odoo/static/src/responsive-snake/example/js/functions.js
+++ website_pinetwork_games_odoo/static/src/responsive-snake/example/js/functions.js
@@ -4,139 +4,140 @@ var accessToken = "";
 var passkey = "";
 const Pi = window.Pi;
 
-function set_points_v2(points) {
-    if(pi_user_id != "" && pi_user_code != "")
-    {
-        var data = {
-            'pi_user_id': pi_user_id,
-            'pi_user_code': pi_user_code,
-            'points': points,
-            'app_client': 'auth_snake',
-            'passkey': passkey,
-            'accessToken': accessToken,
-            'csrf_token': odoo.csrf_token,
-        };
-        $.ajaxSetup({async: false});
-        return $.post( "/pi-points", data).done(function(data) {
-            data = JSON.parse(data);
-            if(data.result && points > 0)
-                alert("You won " + points + " points");
-        }).fail(function() {
-            
-        });
-    }
-}
-
-function set_points(points) {
-    try {
-        // Identify the user with their username / unique network-wide ID, and get permission to request payments from them.
-        const scopes = ['username', 'payments'];
-        function onIncompletePaymentFound(payment) {
-            var data={
-                    'action': 'complete',
-                    'paymentId': payment.identifier,
-                    'txid': payment.transaction.txid,
-                    'app_client': 'auth_snake',
-                    'accessToken': accessToken,
-                    'pi_user_code': pi_user_code,
-                    'pi_user_id': pi_user_id,
-                    'csrf_token': odoo.csrf_token,
-                };
-            
-            return $.post( "/pi-api", data).done(function(data) {
-                                $("#button_click").prop( "disabled", false );
-                                try {
-                                    data = JSON.parse(data);
-                                    if(data.result && data.completed)
-                                    {
-                                        alert("A payment was registered. Reload the page to view the changes.");
-                                    }
-                                } catch (e) {
-                                }
-                            }).fail(function() {
-                                $("#button_click").prop( "disabled", false );
-                            });
-        }; // Read more about this in the SDK reference
-
-        Pi.authenticate(scopes, onIncompletePaymentFound).then(function(auth) {
-            pi_user_id = auth.user.uid;
-            pi_user_code = auth.user.username;
-            accessToken = auth.accessToken;
-            
-            //get_user(false);
-            set_points_v2(points);
-            get_user(true);
-            
-          $( "#button_click" ).click(function() {
-                if(parseFloat($("#pi_donate").val()) > 0)
-                {
-                    $("#button_click").prop( "disabled", true );
-                    /*setTimeout(function ()
-                    {
-                        $("#button_click").prop( "disabled", false );
-                    }, 10000);*/
-                    transfer();
-                }
-                //alert("Click");
-            });
-        }).catch(function(error) {
-            //Pi.openShareDialog("Error", error);
-            //alert(err);
-            console.error(error);
-        });
-    } catch (err) {
-        //Pi.openShareDialog("Error", err);
-        //alert(err);
-        console.error(err);
-        // Not able to fetch the user
-    }
-}
-
-function get_user(pause) {
-    if(pi_user_id != "" && pi_user_code != "")
-    {
-        var data = {
-                    'pi_user_id': pi_user_id,
-                    'pi_user_code': pi_user_code,
-                    'accessToken': accessToken,
-                    'csrf_token': odoo.csrf_token,
-                };
-        $.ajaxSetup({async: false});
-        return $.post( "/get-user", data).done(function(data) {
-            data = JSON.parse(data);
-            if(data.result)
-            {
-                passkey=data.passkey;
-                if(data.unblocked)
-                {
-                    $("#pause").show();
-                    $("#reset").show();
-                    $("#pi_donate").hide();
-                    $("#button_click").hide();
-                    
-                    if(pause)
-                        $("#pause").click();
-                }
-                else
-                {
-                    $("#pause").hide();
-                    $("#pi_donate").hide();
-                    $("#button_click").show();
-                }
-            }
-        }).fail(function() {
-            
-        });
-    }
-}
-
 $( document ).ready(function() {
+    
+    Pi.init({ version: "2.0", sandbox: $("#sandbox").val() });
+    
+    function set_points_v2(points) {
+        if(pi_user_id != "" && pi_user_code != "")
+        {
+            var data = {
+                'pi_user_id': pi_user_id,
+                'pi_user_code': pi_user_code,
+                'points': points,
+                'app_client': 'auth_snake',
+                'passkey': passkey,
+                'accessToken': accessToken,
+                'csrf_token': odoo.csrf_token,
+            };
+            $.ajaxSetup({async: false});
+            return $.post( "/pi-points", data).done(function(data) {
+                data = JSON.parse(data);
+                if(data.result && points > 0)
+                    alert("You won " + points + " points");
+            }).fail(function() {
+                
+            });
+        }
+    }
+
+    function set_points(points) {
+        try {
+            // Identify the user with their username / unique network-wide ID, and get permission to request payments from them.
+            const scopes = ['username', 'payments'];
+            function onIncompletePaymentFound(payment) {
+                var data={
+                        'action': 'complete',
+                        'paymentId': payment.identifier,
+                        'txid': payment.transaction.txid,
+                        'app_client': 'auth_snake',
+                        'accessToken': accessToken,
+                        'pi_user_code': pi_user_code,
+                        'pi_user_id': pi_user_id,
+                        'csrf_token': odoo.csrf_token,
+                    };
+                
+                return $.post( "/pi-api", data).done(function(data) {
+                                    $("#button_click").prop( "disabled", false );
+                                    try {
+                                        data = JSON.parse(data);
+                                        if(data.result && data.completed)
+                                        {
+                                            alert("A payment was registered. Reload the page to view the changes.");
+                                        }
+                                    } catch (e) {
+                                    }
+                                }).fail(function() {
+                                    $("#button_click").prop( "disabled", false );
+                                });
+            }; // Read more about this in the SDK reference
+
+            Pi.authenticate(scopes, onIncompletePaymentFound).then(function(auth) {
+                pi_user_id = auth.user.uid;
+                pi_user_code = auth.user.username;
+                accessToken = auth.accessToken;
+                
+                //get_user(false);
+                set_points_v2(points);
+                get_user(true);
+                
+              $( "#button_click" ).click(function() {
+                    if(parseFloat($("#pi_donate").val()) > 0)
+                    {
+                        $("#button_click").prop( "disabled", true );
+                        /*setTimeout(function ()
+                        {
+                            $("#button_click").prop( "disabled", false );
+                        }, 10000);*/
+                        transfer();
+                    }
+                    //alert("Click");
+                });
+            }).catch(function(error) {
+                //Pi.openShareDialog("Error", error);
+                //alert(err);
+                console.error(error);
+            });
+        } catch (err) {
+            //Pi.openShareDialog("Error", err);
+            //alert(err);
+            console.error(err);
+            // Not able to fetch the user
+        }
+    }
+
+    function get_user(pause) {
+        if(pi_user_id != "" && pi_user_code != "")
+        {
+            var data = {
+                        'pi_user_id': pi_user_id,
+                        'pi_user_code': pi_user_code,
+                        'accessToken': accessToken,
+                        'csrf_token': odoo.csrf_token,
+                    };
+            $.ajaxSetup({async: false});
+            return $.post( "/get-user", data).done(function(data) {
+                data = JSON.parse(data);
+                if(data.result)
+                {
+                    passkey=data.passkey;
+                    if(data.unblocked)
+                    {
+                        $("#pause").show();
+                        $("#reset").show();
+                        $("#pi_donate").hide();
+                        $("#button_click").hide();
+                        
+                        if(pause)
+                            $("#pause").click();
+                    }
+                    else
+                    {
+                        $("#pause").hide();
+                        $("#pi_donate").hide();
+                        $("#button_click").show();
+                    }
+                }
+            }).fail(function() {
+                
+            });
+        }
+    }
     
     $(document).ajaxStop(function() {
                 $("#loading_word").hide();
             });
-    
-    Pi.init({ version: "2.0", sandbox: $("#sandbox").val() });
+
     
     async function auth() {
         $("#pause").hide();
