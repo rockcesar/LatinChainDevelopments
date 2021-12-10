635dc28e290d49117c5a09f3f46401e6a54294f2
ccordero <cesar.cordero.r@gmail.com>
2021-12-10 10:55:57 -0400

    Upgrade

--- website_pinetwork_odoo/controllers/main.py
+++ website_pinetwork_odoo/controllers/main.py
@@ -275,6 +275,6 @@ class PiNetworkBaseController(http.Controller):
         
         return json.dumps({'draw': int(draw), 'aaData': data, "iTotalRecords": pi_users_count, "iTotalDisplayRecords": pi_users_count_filter})
         
-    @http.route('/validation.txt', type='http', auth="public", website=True, csrf=False)
+    @http.route('/validation-key.txt', type='http', auth="public", website=True, csrf=False)
     def validation_txt(self, **kw):
         return "440b3e77563e4151952010f7a7768ae060bb0408c33604322acca070824ce31fc8bdb73cc819e4acd4600d7f8aabce29ff4242fb00a1ae79bca414c11b7ad0b4"
