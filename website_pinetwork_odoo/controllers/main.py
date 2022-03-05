6b46732d1d9b7f731fed75cef5fe3ddf097d159d
ccordero <cesar.cordero.r@gmail.com>
2022-03-05 13:28:40 -0400

    Upgrade

--- website_pinetwork_odoo/controllers/main.py
+++ website_pinetwork_odoo/controllers/main.py
@@ -49,10 +49,22 @@ class PiNetworkBaseController(http.Controller):
             sandbox = admin_app_list[0].sandbox
         
         return http.request.render('website_pinetwork_odoo.example', {'sandbox': sandbox})
-    
+        
     @http.route('/get-external-user', type='http', auth="public", website=True, csrf=False, methods=['POST'])
     def get_external_user(self, **kw):
         pi_users_list = request.env["pi.users"].sudo().search([('pi_user_code', '=', kw['pi_user_code'])])
+        
+        if len(pi_users_list) == 0:
+            return json.dumps({'result': False})
+        
+        return json.dumps({'result': True, 'pi_user_code': pi_users_list[0].pi_user_code,
+                            'points': pi_users_list[0].points, 'points_chess': pi_users_list[0].points_chess, 
+                            'points_sudoku': pi_users_list[0].points_sudoku,
+                            'points_snake': pi_users_list[0].points_snake, 'unblocked': pi_users_list[0].unblocked})
+    
+    @http.route('/get-external-user/<string:pi_user_code>', type='http', auth="public", website=True, csrf=False, methods=['GET'])
+    def get_external_user(self, pi_user_code, **kw):
+        pi_users_list = request.env["pi.users"].sudo().search([('pi_user_code', '=', pi_user_code)])
         
         if len(pi_users_list) == 0:
             return json.dumps({'result': False})
