f0f4036f21e3c6bdb7517b97f942818dde0e849e
ccordero <cesar.cordero.r@gmail.com>
2022-03-19 08:09:20 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions.js
+++ website_pinetwork_games_odoo/static/src/js/functions.js
@@ -108,7 +108,7 @@ $( document ).ready(function() {
                 set_points(0).always(function(){
                     get_user(false).always(function(){
                         $( "#button_click" ).click(function() {
-                            if(parseFloat($("#pi_donate").val()) >= 1)
+                            if(parseFloat($("#pi_donate").val()) >= parseFloat($("#amount").val()))
                             {
                                 $("#button_click").prop( "disabled", true );
                                 /*setTimeout(function ()
