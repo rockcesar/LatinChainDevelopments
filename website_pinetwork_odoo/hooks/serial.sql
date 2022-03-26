bea266f952e25438fde60c9a97a1192e2ccdcf33
ccordero <cesar.cordero.r@gmail.com>
2022-03-26 05:39:10 -0400

    Upgrade

--- website_pinetwork_odoo/hooks/serial.sql
+++ website_pinetwork_odoo/hooks/serial.sql
@@ -6,5 +6,8 @@ alter sequence admin_apps_id_seq as bigint;
 alter sequence pi_users_id_seq as bigint;
 
 /*
+SELECT setval('pi_transactions_id_seq', (SELECT max(id) FROM pi_transactions));
+UPDATE pi_transactions SET id=DEFAULT;
 ALTER SEQUENCE pi_transactions_id_seq RESTART;
+UPDATE pi_transactions SET id=DEFAULT;
 */
