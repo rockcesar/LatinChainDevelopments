11b8d40b4fd00ce62ba2a879d9aed91f083c1a19
César Cordero <cesar.cordero.r@gmail.com>
2022-09-20 19:19:31 -0400

    Upgrading

--- website_pinetwork_odoo/models/admin_apps.py
+++ website_pinetwork_odoo/models/admin_apps.py
@@ -180,7 +180,7 @@ class admin_apps(models.Model):
             point_list.append(i.id)
         
         for i in self:
-            transactions_domain = [('action', '=', 'complete'), ('create_date', '>=', datetime.now() - timedelta(days=30))]
+            transactions_domain = [('action', '=', 'complete'), ('create_date', '>=', datetime.now() - timedelta(days=28))]
 
             transactions_ids = self.env["pi.transactions"].search(transactions_domain)
 
