1c9124e3e053947ec76423bd144040a2d4ab8a7a
César Cordero <cesar.cordero.r@gmail.com>
2022-09-25 11:54:55 -0400

    Upgrading

--- website_pinetwork_odoo/controllers/main.py
+++ website_pinetwork_odoo/controllers/main.py
@@ -374,7 +374,7 @@ class PiNetworkBaseController(http.Controller):
         
         pi_user = request.env["pi.users"].sudo().search([('pi_user_code', '=', pi_user_code)])
         
-        return http.request.render('website_pinetwork_odoo.list_winners_zone', {'pi_users_verified_count': pi_users_verified_count, 'pi_users_count': pi_users_count, 'pi_users_list': pi_users_list, 'pi_users_winners_datetime': pi_users_winners_datetime, 'pi_user': pi_user})
+        return http.request.render('website_pinetwork_odoo.list_winners_zone', {'pi_users_winners_to_pay': pi_users_winners_to_pay, 'pi_users_verified_count': pi_users_verified_count, 'pi_users_count': pi_users_count, 'pi_users_list': pi_users_list, 'pi_users_winners_datetime': pi_users_winners_datetime, 'pi_user': pi_user})
         
     @http.route('/get-credits/', type='http', auth="public", website=True)
     def get_credits(self, **kw):
