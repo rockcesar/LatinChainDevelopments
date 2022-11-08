19947b394b82f87684d63a26aa810f1040d5a291
César Cordero <cesar.cordero.r@gmail.com>
2022-11-08 15:01:13 -0400

    Upgrading

--- website_pinetwork_odoo/controllers/main.py
+++ website_pinetwork_odoo/controllers/main.py
@@ -130,7 +130,7 @@ class PiNetworkBaseController(http.Controller):
                             'points_snake': pi_users_list[0].points_snake, 'points_datetime': str(pi_users_list[0].points_datetime) + " UTC",
                             'unblocked': pi_users_list[0].unblocked, 'is_winner': is_winner}), headers=headers)
     
-    @http.route('/get-user', type='http', auth="public", website=True, csrf=False, methods=['POST'])
+    @http.route('/get-user', type='http', auth="public", website=True, csrf=True, methods=['POST'])
     def get_user(self, **kw):
         re = requests.get('https://api.minepi.com/v2/me',data={},json={},headers={'Authorization': "Bearer " + kw['accessToken']})
         
@@ -177,7 +177,7 @@ class PiNetworkBaseController(http.Controller):
                             'passkey': passkey,
                             'im_winner': im_winner})
         
-    @http.route('/pi-api', type='http', auth="public", website=True, csrf=False, methods=['POST'])
+    @http.route('/pi-api', type='http', auth="public", website=True, csrf=True, methods=['POST'])
     def pi_api(self, **kw):
         admin_apps_block_list = request.env["admin.apps"].sudo().search([('app', '=', "auth_platform"), ('block_points', '=', True)])
         
@@ -203,7 +203,7 @@ class PiNetworkBaseController(http.Controller):
         
         return request.env["admin.apps"].pi_api(kw)
         
-    @http.route('/pi-points', type='http', auth="public", website=True, csrf=False, methods=['POST'])
+    @http.route('/pi-points', type='http', auth="public", website=True, csrf=True, methods=['POST'])
     def pi_points(self, **kw):
         """
         admin_apps_block_list = request.env["admin.apps"].sudo().search([('app', '=', "auth_platform"), ('block_points', '=', True)])
