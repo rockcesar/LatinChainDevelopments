335e32873d7732d352f8e04ca637b3d6d72f3992
César Cordero <cesar.cordero.r@gmail.com>
2022-12-16 10:46:02 -0400

    Upgrading

--- website_pinetwork_games_odoo/static/src/pidoku/functions.js
+++ website_pinetwork_games_odoo/static/src/pidoku/functions.js
@@ -51,20 +51,18 @@ function get_user() {
                     $("#pi_donate").hide();
                     $("#button_click").hide();
                     $("#sudoku-tab").show();
+                    $("#counter_span").html("(5)");
                     setTimeout(function() {
-                        $("#counter_span").html("(5)");
+                        $("#counter_span").html("(4)");
                         setTimeout(function() {
-                            $("#counter_span").html("(4)");
+                            $("#counter_span").html("(3)");
                             setTimeout(function() {
-                                $("#counter_span").html("(3)");
+                                $("#counter_span").html("(2)");
                                 setTimeout(function() {
-                                    $("#counter_span").html("(2)");
+                                    $("#counter_span").html("(1)");
                                     setTimeout(function() {
-                                        $("#counter_span").html("(1)");
-                                        setTimeout(function() {
-                                            $("#counter_span").html("");
-                                            $("#sudoku-tab").click();
-                                        }, 1000);
+                                        $("#counter_span").html("");
+                                        $("#sudoku-tab").click();
                                     }, 1000);
                                 }, 1000);
                             }, 1000);
