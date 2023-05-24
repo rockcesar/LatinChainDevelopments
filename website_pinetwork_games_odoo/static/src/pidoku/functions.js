42438efc10e15adcc3b5674afb361aee104a8fa7
Cesar Cordero <cesar.cordero.r@gmail.com>
2023-05-24 07:30:36 -0400

    Upgrading

--- website_pinetwork_games_odoo/static/src/pidoku/functions.js
+++ website_pinetwork_games_odoo/static/src/pidoku/functions.js
@@ -69,10 +69,10 @@ function get_user() {
 
 $( document ).ready(function() {
     
-    $(document).blur(function(){
+    /*$(document).blur(function(){
         alert("You changed of window, puzzle will be cleared.");
         clear_this_board();
-    });
+    });*/
     
     $('.timer').countimer({
 			autoStart : false
