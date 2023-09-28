7230eb62ef4c3ad20055c9f7259c564427b61d8f
Cesar Cordero <cesar.cordero.r@gmail.com>
2023-09-28 05:46:42 -0400

    Upgrading

--- Pi_A2U_payment/pi_python.py
+++ Pi_A2U_payment/pi_python.py
@@ -7,7 +7,7 @@ import requests
 import json
 import stellar_sdk as s_sdk
 
-class PiNetwork:    
+class PiNetwork:
      
     api_key = ""
     client = ""
@@ -234,11 +234,11 @@ class PiNetwork:
             return False
         elif "metadata" not in data:
             return False
-        elif "uid" not in data:
+        elif "user_uid" not in data:
             return False
         elif "identifier" not in data:
             return False
-        elif "recipient" not in data:
+        elif "to_address" not in data:
             return False
         return True
 
@@ -248,63 +248,3 @@ class PiNetwork:
         elif len(seed) != 56:
             return False
         return True
-
-""" 
-    Your SECRET Data 
-    Visit the Pi Developer Portal to get these data
-    
-     DO NOT expose these values to public
-"""
-api_key = "Enter Here Your API Key" 
-wallet_private_seed = "SecretWalletSeed" 
-
-""" Initialization """
-pi = PiNetwork()
-pi.initialize(api_key, wallet_private_seed, "Pi Testnet")
-
-""" 
-    Example Data
-    Get the user_uid from the Frontend
-"""
-user_uid = "GET-THIS-SECRET-DATA-FROMFRONTEND" #unique for every user
-
-
-""" Build your payment """
-payment_data = {
-  "amount": 1,
-  "memo": "Test - Greetings from MyApp",
-  "metadata": {"internal_data": "My favorite ice creame"},
-  "uid": user_uid
-}
-
-""" Check for incomplete payments """
-incomplete_payments = pi.get_incomplete_server_payments()
-
-if __debug__:
-    if len(incomplete_payments) > 0:
-        print("Found incomplete payments: ")
-        print(str(incomplete_payments))
-
-""" Handle incomplete payments first """
-if len(incomplete_payments) > 0:
-    for i in incomplete_payments:
-        if i["transaction"] == None:
-            txid = pi.submit_payment(i["identifier"], i)
-            pi.complete_payment(i["identifier"], txid)
-        else:
-            pi.complete_payment(i["identifier"], i["transaction"]["txid"])
-
-""" Create an payment """
-payment_id = pi.create_payment(payment_data)
-
-""" 
-    Submit the payment and receive the txid
-    
-    Store the txid on your side!
-"""
-if payment_id and len(payment_id) > 0:
-    txid = pi.submit_payment(payment_id, False)
-
-    """ Complete the Payment """
-    if txid and len(txid) > 0:
-        payment = pi.complete_payment(payment_id, txid)
