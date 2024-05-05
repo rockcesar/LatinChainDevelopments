a055a56dfab5dcd4b70bb9016ec354050396c08e
Cesar Cordero <cesar.cordero.r@gmail.com>
2024-05-05 15:07:07 -0400

    Upgrading

--- website_pinetwork_odoo/static/src/js/common_functions.js
+++ website_pinetwork_odoo/static/src/js/common_functions.js
@@ -1,7 +1,7 @@
 function validateYouTubeUrl(url)
 {
     if (url) {
-        var regExp = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+        var regExp = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|live\/|shorts\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
         var match = url.match(regExp);
         if(match) {
             return match;
