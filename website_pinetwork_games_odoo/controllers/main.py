b249489d89d9fdc9bee02a851ad181ca3e639f97
Cesar Cordero <cesar.cordero.r@gmail.com>
2024-05-06 03:47:35 -0400

    Upgrading

--- website_pinetwork_games_odoo/controllers/main.py
+++ website_pinetwork_games_odoo/controllers/main.py
@@ -39,6 +39,7 @@ class Website(Website):
             no_footer = True
             total_transactions_daily_count = 0
             total_users_daily_count = 0
+            pioneers_streaming = False
         else:
             sandbox = admin_app_list[0].sandbox
             amount = admin_app_list[0].amount
@@ -56,8 +57,9 @@ class Website(Website):
             no_footer = True
             total_transactions_daily_count = int(admin_app_list[0].total_transactions_daily_count)
             total_users_daily_count = int(admin_app_list[0].total_users_daily_count)
+            pioneers_streaming = admin_app_list[0].pioneers_streaming
         
-        return http.request.render('website_pinetwork_games_odoo.mainpage', {'total_transactions_daily_count': total_transactions_daily_count, 'total_users_daily_count': total_users_daily_count, 'no_footer': True, 'mainnet': mainnet, 'sandbox': sandbox, 'amount': amount, 'google_adsense': google_adsense, 'a_ads': a_ads, 'a_ads_data': a_ads_data, 'a_ads_style': a_ads_style, 'a_ads_2': a_ads_2, 'a_ads_data_2': a_ads_data_2, 'a_ads_style_2': a_ads_style_2, 'a_ads_3': a_ads_3, 'a_ads_data_3': a_ads_data_3, 'a_ads_style_3': a_ads_style_3})
+        return http.request.render('website_pinetwork_games_odoo.mainpage', {'pioneers_streaming': pioneers_streaming, 'total_transactions_daily_count': total_transactions_daily_count, 'total_users_daily_count': total_users_daily_count, 'no_footer': True, 'mainnet': mainnet, 'sandbox': sandbox, 'amount': amount, 'google_adsense': google_adsense, 'a_ads': a_ads, 'a_ads_data': a_ads_data, 'a_ads_style': a_ads_style, 'a_ads_2': a_ads_2, 'a_ads_data_2': a_ads_data_2, 'a_ads_style_2': a_ads_style_2, 'a_ads_3': a_ads_3, 'a_ads_data_3': a_ads_data_3, 'a_ads_style_3': a_ads_style_3})
 
 class PiNetworkController(http.Controller):
     @http.route('/radioforus', type='http', auth="public", website=True)
