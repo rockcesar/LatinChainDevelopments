1ac0ab61f17938cc65f97bd3dde673a3b68fd360
Cesar Cordero <cesar.cordero.r@gmail.com>
2024-06-27 16:57:28 -0400

    Upgrading

--- website_pinetwork_games_odoo/static/src/pidoku/functions.js
+++ website_pinetwork_games_odoo/static/src/pidoku/functions.js
@@ -61,7 +61,7 @@ function set_points(points) {
             {
                 end();
                 $("#gained_points").show();
-                $("#gained_points").html("+" + points);
+                $("#gained_points").html("+" + points + " <i class='fa fa-level-up'></i>");
                 setTimeout(function() {
                     $("#gained_points").hide();
                 }, 15000);
