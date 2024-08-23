7863121061140cf89eef5b5654659a3ab85e111d
Cesar Cordero <cesar.cordero.r@gmail.com>
2024-08-23 05:54:52 -0400

    Upgrading

--- website_pinetwork_games_odoo/controllers/main.py
+++ website_pinetwork_games_odoo/controllers/main.py
@@ -205,6 +205,41 @@ class PiNetworkController(http.Controller):
         
         return http.request.render('website_pinetwork_games_odoo.mahjong', {'sandbox': sandbox, 'mainnet': mainnet, 'google_adsense': google_adsense})
     
+    @http.route('/latinchain-mainnet-redirect', type='http', auth="public", website=True)
+    def latinchain_mainnet_redirect(self, **kw):
+        admin_app_list = request.env["admin.apps"].sudo().search([('app', '=', 'auth_platform')])
+        
+        if len(admin_app_list) == 0:
+            sandbox = False
+            amount = False
+            google_adsense = ""
+            a_ads = ""
+            a_ads_data = ""
+            a_ads_style = ""
+            a_ads_2 = ""
+            a_ads_data_2 = ""
+            a_ads_style_2 = ""
+            a_ads_3 = ""
+            a_ads_data_3 = ""
+            a_ads_style_3 = ""
+            mainnet = ""
+        else:
+            sandbox = admin_app_list[0].sandbox
+            amount = admin_app_list[0].amount
+            google_adsense = admin_app_list[0].google_adsense
+            a_ads = admin_app_list[0].a_ads
+            a_ads_data = admin_app_list[0].a_ads_data
+            a_ads_style = admin_app_list[0].a_ads_style
+            a_ads_2 = admin_app_list[0].a_ads_2
+            a_ads_data_2 = admin_app_list[0].a_ads_data_2
+            a_ads_style_2 = admin_app_list[0].a_ads_style_2
+            a_ads_3 = admin_app_list[0].a_ads_3
+            a_ads_data_3 = admin_app_list[0].a_ads_data_3
+            a_ads_style_3 = admin_app_list[0].a_ads_style_3
+            mainnet = admin_app_list[0].mainnet
+        
+        return http.request.render('website_pinetwork_games_odoo.latinchain_mainnet_redirect', {'mainnet': mainnet, 'sandbox': sandbox, 'amount': amount, 'google_adsense': google_adsense, 'a_ads': a_ads, 'a_ads_data': a_ads_data, 'a_ads_style': a_ads_style, 'a_ads_2': a_ads_2, 'a_ads_data_2': a_ads_data_2, 'a_ads_style_2': a_ads_style_2, 'a_ads_3': a_ads_3, 'a_ads_data_3': a_ads_data_3, 'a_ads_style_3': a_ads_style_3})
+    
     @http.route('/latinchain_x', type='http', auth="public", website=True)
     def latinchain_x(self, **kw):
         admin_app_list = request.env["admin.apps"].sudo().search([('app', '=', 'auth_platform')])
