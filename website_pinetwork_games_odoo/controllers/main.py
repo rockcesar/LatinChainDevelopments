c2c5e4257f023cd3142c3db849d7aab540d813f3
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-01-01 23:29:03 -0400

    Upgrading

--- website_pinetwork_games_odoo/controllers/main.py
+++ website_pinetwork_games_odoo/controllers/main.py
@@ -42,6 +42,7 @@ class Website(Website):
             pi_main_user = False
             points_latin_amount = 1
             pi_ad_hours = 0
+            pi_ad_max = 0
         else:
             sandbox = admin_app_list[0].sandbox
             amount = admin_app_list[0].amount
@@ -63,12 +64,13 @@ class Website(Website):
             pi_main_user = admin_app_list[0].pi_main_user
             points_latin_amount = admin_app_list[0].points_latin_amount
             pi_ad_hours = admin_app_list[0].pi_ad_seconds/3600
+            pi_ad_max = admin_app_list[0].pi_ad_max
 
         nopopup = False
         if "nopopup" in kw:
             nopopup = kw["nopopup"]
         
-        return http.request.render('website_pinetwork_games_odoo.mainpage', {'nopopup': nopopup, 'pi_ad_hours': int(pi_ad_hours), 'points_latin_amount': points_latin_amount, 'pi_main_user': pi_main_user, 'pioneers_streaming': pioneers_streaming, 'total_transactions_daily_count': total_transactions_daily_count, 'total_users_daily_count': total_users_daily_count, 'no_footer': True, 'mainnet': mainnet, 'sandbox': sandbox, 'amount': amount, 'google_adsense': google_adsense, 'a_ads': a_ads, 'a_ads_data': a_ads_data, 'a_ads_style': a_ads_style, 'a_ads_2': a_ads_2, 'a_ads_data_2': a_ads_data_2, 'a_ads_style_2': a_ads_style_2, 'a_ads_3': a_ads_3, 'a_ads_data_3': a_ads_data_3, 'a_ads_style_3': a_ads_style_3})
+        return http.request.render('website_pinetwork_games_odoo.mainpage', {'nopopup': nopopup, 'pi_ad_max': pi_ad_max, 'pi_ad_hours': int(pi_ad_hours), 'points_latin_amount': points_latin_amount, 'pi_main_user': pi_main_user, 'pioneers_streaming': pioneers_streaming, 'total_transactions_daily_count': total_transactions_daily_count, 'total_users_daily_count': total_users_daily_count, 'no_footer': True, 'mainnet': mainnet, 'sandbox': sandbox, 'amount': amount, 'google_adsense': google_adsense, 'a_ads': a_ads, 'a_ads_data': a_ads_data, 'a_ads_style': a_ads_style, 'a_ads_2': a_ads_2, 'a_ads_data_2': a_ads_data_2, 'a_ads_style_2': a_ads_style_2, 'a_ads_3': a_ads_3, 'a_ads_data_3': a_ads_data_3, 'a_ads_style_3': a_ads_style_3})
 
 class PiNetworkController(http.Controller):
     @http.route('/radioforus', type='http', auth="public", website=True)
@@ -303,6 +305,7 @@ class PiNetworkController(http.Controller):
             mainnet = ""
             points_latin_amount = 1
             pi_ad_hours = 0
+            pi_ad_max = 0
         else:
             sandbox = admin_app_list[0].sandbox
             amount = admin_app_list[0].amount
@@ -319,8 +322,9 @@ class PiNetworkController(http.Controller):
             mainnet = admin_app_list[0].mainnet
             points_latin_amount = admin_app_list[0].points_latin_amount
             pi_ad_hours = admin_app_list[0].pi_ad_seconds/3600
+            pi_ad_max = admin_app_list[0].pi_ad_max
         
-        return http.request.render('website_pinetwork_games_odoo.pinetwork', {'pi_ad_hours': int(pi_ad_hours), 'points_latin_amount': points_latin_amount, 'mainnet': mainnet, 'sandbox': sandbox, 'amount': amount, 'google_adsense': google_adsense, 'a_ads': a_ads, 'a_ads_data': a_ads_data, 'a_ads_style': a_ads_style, 'a_ads_2': a_ads_2, 'a_ads_data_2': a_ads_data_2, 'a_ads_style_2': a_ads_style_2, 'a_ads_3': a_ads_3, 'a_ads_data_3': a_ads_data_3, 'a_ads_style_3': a_ads_style_3})
+        return http.request.render('website_pinetwork_games_odoo.pinetwork', {'pi_ad_max': pi_ad_max, 'pi_ad_hours': int(pi_ad_hours), 'points_latin_amount': points_latin_amount, 'mainnet': mainnet, 'sandbox': sandbox, 'amount': amount, 'google_adsense': google_adsense, 'a_ads': a_ads, 'a_ads_data': a_ads_data, 'a_ads_style': a_ads_style, 'a_ads_2': a_ads_2, 'a_ads_data_2': a_ads_data_2, 'a_ads_style_2': a_ads_style_2, 'a_ads_3': a_ads_3, 'a_ads_data_3': a_ads_data_3, 'a_ads_style_3': a_ads_style_3})
     
     @http.route('/sudoku', type='http', auth="public", website=True)
     def sudoku(self, **kw):
