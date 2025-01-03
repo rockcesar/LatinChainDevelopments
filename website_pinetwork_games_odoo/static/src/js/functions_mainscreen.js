aa526d1e40e7c61d1ae83d409b6c0ae4dd06bd5c
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-01-03 18:13:17 -0400

    Upgrading

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -997,9 +997,4 @@ $( document ).ready(function() {
         }
     });
     
-    $('body').tooltip({
-        selector: '[data-bs-toggle="tooltip"]',
-        trigger:'hover focus click manual'
-    });
-    
 });
