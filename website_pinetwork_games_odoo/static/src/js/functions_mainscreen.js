26325d80b825e41c42ac1335f021e6e455975fb7
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-05-15 13:06:17 -0400

    Upgrading

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -227,7 +227,7 @@ function get_user() {
                 $(".show-15-puzzle").show();
                 $(".show-tvonline").show();
                 //$(".show-sport-results").show();
-                //$(".show-cointelegraph").show();
+                $(".show-cointelegraph").show();
                 $(".show-streamerzoneboard").show();
                 //$(".show-stellarium").show();
                 //$(".show-latin-search").show();
