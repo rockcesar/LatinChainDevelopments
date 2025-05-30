0903c04ed57b80f409a9bc5a65024ddb606c45eb
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-05-30 19:52:01 -0400

    Upgrading

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -633,37 +633,39 @@ $( document ).ready(function() {
         return false;
     });
     
-    $("#exchange_latin > option").each(function() {
-        if(location.pathname.substring(0, 3) == "/es")
-        {
-            var value = $(this).val();
-            if(value == "Chess10x1")
+    if(["Mainnet ON", "Mainnet OFF"].includes($("#mainnet").val()))
+    {
+        $("#exchange_latin > option").each(function() {
+            if(location.pathname.substring(0, 3) == "/es")
             {
-                $(this).text("Paga 10 Latin points, gana 1 Chess points");
+                var value = $(this).val();
+                if(value == "Chess10x1")
+                {
+                    $(this).text("Paga 10 Latin points, gana 1 Chess points");
+                }
+                else if(value == "Sudoku10x3")
+                {
+                    $(this).text("Paga 10 Latin points, gana 3 Sudoku points");
+                }
+                else if(value == "Snake10x1")
+                {
+                    $(this).text("Paga 10 Latin points, gana 1 Snake points");
+                }
+                else if(value == "Chess100x10")
+                {
+                    $(this).text("Paga 10 Latin points, gana 10 Chess points");
+                }
+                else if(value == "Sudoku100x12")
+                {
+                    $(this).text("Paga 10 Latin points, gana 12 Sudoku points");
+                }
+                else if(value == "Snake100x10")
+                {
+                    $(this).text("Paga 10 Latin points, gana 10 Snake points");
+                }
             }
-            else if(value == "Sudoku10x3")
-            {
-                $(this).text("Paga 10 Latin points, gana 3 Sudoku points");
-            }
-            else if(value == "Snake10x1")
-            {
-                $(this).text("Paga 10 Latin points, gana 1 Snake points");
-            }
-            else if(value == "Chess100x10")
-            {
-                $(this).text("Paga 10 Latin points, gana 10 Chess points");
-            }
-            else if(value == "Sudoku100x12")
-            {
-                $(this).text("Paga 10 Latin points, gana 12 Sudoku points");
-            }
-            else if(value == "Snake100x10")
-            {
-                $(this).text("Paga 10 Latin points, gana 10 Snake points");
-            }
-        }
-    });
-    
+        });
+    }
     
     $(".open_streaming").click(function(){
         var streaming_url = validateYouTubeUrl($(this).attr("videourl"));
