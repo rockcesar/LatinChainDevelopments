fa4c528c4398be99f36e1aa92222ba63ce9cc349
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-07-21 14:10:35 -0400

    Upgrading

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -22,9 +22,11 @@ async function colorboxLoaded()
     {
         if($.colorbox && !colorbox_opened)
         {
-            $.colorbox({href:"/latinchain-mainnet-redirect", closeButton:false, overlayClose:false, escKey:false, iframe:true, width: "100%", height: "100%", maxWidth: "100%", maxHeight: "100%"});
-            colorbox_opened = true;
-            return false;
+            setTimeout(function() {
+                $.colorbox({href:"/latinchain-mainnet-redirect", closeButton:false, overlayClose:false, escKey:false, iframe:true, width: "100%", height: "100%", maxWidth: "100%", maxHeight: "100%"});
+                colorbox_opened = true;
+                return true;
+            }, 5000);
         }else{
             if(colorbox_opened)
                 return false;
@@ -629,7 +631,7 @@ async function showPiAds(Pi) {
 }
 
 $( document ).ready(function() {
-    colorboxLoaded();
+    //colorboxLoaded();
     
     window.addEventListener('unhandledrejection', function (e) {
         e.preventDefault();
@@ -697,6 +699,15 @@ $( document ).ready(function() {
         window.location.href="#pay_with";
     });
     */
+    
+    $(".open_mainnet_redirect").click(function(){
+        colorboxLoaded();
+    });
+    
+    if(["Testnet ON", "Testnet OFF"].includes($("#mainnet").val()) && $("#nopopup").val() == false)
+    {
+        $(".open_mainnet_redirect").click();
+    }
     
     $(".open_streaming").click(function(){
         var streaming_url = validateYouTubeUrl($(this).attr("videourl"));
