f46e23278d90fcd72032880bddb96eaec311b43b
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-08-19 11:11:11 -0400

    Upgrading

--- website_pinetwork_odoo/static/src/js/common_functions.js
+++ website_pinetwork_odoo/static/src/js/common_functions.js
@@ -122,6 +122,8 @@ const speechModule = (() => {
                 
                 var voice = "";
                 
+                alert(savedLanguage1);
+                
                 if (savedLanguage1) {
                     voice = voices.find(v => v.lang.startsWith(savedLanguage1)) ||
                             voices.find(v => v.lang.startsWith('en_US')) ||
@@ -229,8 +231,6 @@ async function speak(textIncome){
         var savedLanguage1 = localStorage.getItem('lastTranslateLanguage');
                 
         var voice = "";
-        
-        alert(savedLanguage1);
         
         if (savedLanguage1) {
             voice = voices.find(v => v.lang.startsWith(savedLanguage1)) ||
