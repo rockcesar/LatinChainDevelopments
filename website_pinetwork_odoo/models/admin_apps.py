f7560b15864bd812fe065fa02ae3355d0652cb8e
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-11-23 19:20:01 -0400

    Upgrade

--- website_pinetwork_odoo/models/admin_apps.py
+++ website_pinetwork_odoo/models/admin_apps.py
@@ -254,7 +254,10 @@ class admin_apps(models.Model):
             users_ids = self.env["pi.users"].read_group(users_domain, ['points_latin_daily'], ['pi_user_role'])
             
             if len(users_ids) > 0:
-                total = users_ids[0]['points_latin_daily']
+                cont = 0
+                while cont < len(users_ids):
+                    total = users_ids[cont]['points_latin_daily']
+                    cont+=1
             
             i.points_latin_daily_total = total
   
