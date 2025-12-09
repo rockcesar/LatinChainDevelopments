41056e8763964b865177459bcf753e57849a9492
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-12-09 00:55:55 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/apps/iq_test_functions.js
+++ website_pinetwork_games_odoo/static/src/js/apps/iq_test_functions.js
@@ -447,8 +447,7 @@ function viewLastCertificate() {
 function viewHigherScoreCertificate() {
     const savedData = startCommonAppsAIVars;
     if (savedData) {
-        alert(savedData.iq_date);
-        renderCertificate(savedData.iq_name, savedData.iq_result, savedData.iq_category, new Date(savedData.iq_date).toLocaleDateString());
+        renderCertificate(savedData.iq_name, savedData.iq_result, savedData.iq_category, savedData.iq_date.toLocaleDateString());
         startScreen.classList.add('hidden');
         resultScreen.classList.remove('hidden');
     }
