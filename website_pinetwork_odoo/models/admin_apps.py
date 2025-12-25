5b2de5b0b2c5374d5e5b41e7fc630f7fc127a391
Cesar Cordero <cesar.cordero.r@gmail.com>
2025-12-25 10:21:20 -0400

    Upgrade

--- website_pinetwork_odoo/models/admin_apps.py
+++ website_pinetwork_odoo/models/admin_apps.py
@@ -239,7 +239,7 @@ class admin_apps(models.Model):
     points_latin_daily_total = fields.Integer('Points Latin_daily total', compute="_compute_points_latin_daily_total", store=False, groups="website_pinetwork_odoo.group_pi_admin,base.group_system")
     points_latin_daily_total_notcomputed = fields.Integer('Points Latin_daily total notcomputed', store=True, groups="website_pinetwork_odoo.group_pi_admin,base.group_system")
     discount_active = fields.Boolean('Discount', default=False, groups="website_pinetwork_odoo.group_pi_admin,base.group_system")
-    discount_percentage = fields.Float('Discount percentage', default=50, groups="website_pinetwork_odoo.group_pi_admin,base.group_system")
+    discount_percentage = fields.Float('Discount percentage', digits=(50,7), default=50, groups="website_pinetwork_odoo.group_pi_admin,base.group_system")
     
     """
     @api.depends("amount")
