3b298173ac7b17d1dc2f15660458668708f02ebd
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-02-05 15:44:39 -0400

    Upgrade

--- website_pinetwork_odoo/controllers/main.py
+++ website_pinetwork_odoo/controllers/main.py
@@ -466,7 +466,10 @@ class PiNetworkBaseController(http.Controller):
         else:
             referrer_code = ""
         
-        resting_time = str((pi_users_list[0].pi_ad_datetime-(datetime.now() - timedelta(seconds=pi_ad_seconds)))).split(".")[0]
+        if pi_users_list[0].pi_ad_datetime:
+            resting_time = str((pi_users_list[0].pi_ad_datetime-(datetime.now() - timedelta(seconds=pi_ad_seconds)))).split(".")[0]
+        else:
+            resting_time = ""
         
         pi_ad_counter = pi_users_list[0].pi_ad_counter
         if pi_users_list[0].pi_ad_counter > pi_ad_max:
