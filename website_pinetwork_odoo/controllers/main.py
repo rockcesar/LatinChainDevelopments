454a9c9e48e99c5d0f3ecaa0e950df8f36211c13
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-02-06 19:08:35 -0400

    Upgrade

--- website_pinetwork_odoo/controllers/main.py
+++ website_pinetwork_odoo/controllers/main.py
@@ -445,6 +445,7 @@ class PiNetworkBaseController(http.Controller):
                 pi_ad_new = True
         else:
             pi_ad_new = True
+                
         
         if pi_users_list[0].unblocked:
             if not pi_users_list[0].pi_ad_automatic:
@@ -576,7 +577,7 @@ class PiNetworkBaseController(http.Controller):
                     pi_ad_new = True
             else:
                 values.update({'pi_ad_datetime': datetime.now()})
-                values.update({'pi_ad_counter': 0})
+                values.update({'pi_ad_counter': 1})
                 pi_ad_new = True
         
         #Uncomment in case of you want to save wallet address
@@ -875,7 +876,7 @@ class PiNetworkBaseController(http.Controller):
                     pi_ad_new = True
             else:
                 values.update({'pi_ad_datetime': datetime.now()})
-                values.update({'pi_ad_counter': 0})
+                values.update({'pi_ad_counter': 1})
                 pi_ad_new = True
         
         pi_users_list[0].sudo().write(values)
