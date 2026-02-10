821babf53947a01c82d9e7a090e3be6b2c6be52e
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-02-10 07:59:12 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -1485,7 +1485,7 @@ $( document ).ready(function() {
                             //if(["Mainnet ON", "Mainnet OFF"].includes($("#mainnet").val()))
                             if(show_pi_ad_user && ["Mainnet ON", "Mainnet OFF"].includes($("#mainnet").val()))
                             {
-                                if(confirm("In order to maintain our servers, we ask you to watch a Rewarded Ad, we will give you a reward at the end of the ad. ¿Do you want to watch it?."))
+                                if(confirm("Watch a video to claim your reward?. In order to maintain our servers, we ask you to watch a Rewarded Ad until the end, we will give you a reward at the end of the ad."))
                                     showRewardedPiAd(false);
                                 /*(async () => {
                                     var result_piad = await showPiAds(Pi, true);
