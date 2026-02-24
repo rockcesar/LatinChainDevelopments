e96d3f08bbd6a334912fb281c0ae7faf76a555ad
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-02-24 16:56:14 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -1489,7 +1489,7 @@ $( document ).ready(function() {
                         
                         if(["Mainnet ON", "Mainnet OFF"].includes($("#mainnet").val()))
                         {
-                            if($("#nopopup").val() == false)
+                            if(!unblocked && $("#nopopup").val() == false)
                             {
                                 colorboxLoadedMainnet();
                             }else
