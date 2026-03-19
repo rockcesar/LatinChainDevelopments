0a1312435b35184427817db68fdcc687c580b2fb
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-03-19 19:31:25 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -908,6 +908,8 @@ $( document ).ready(function() {
             //resultPara.textContent = 'MDN shared successfully'
           } catch(err) {
             //alert('Error: ' + err);
+            
+            copyToClipboard($("#share_referrer_latinchain_message").text() + " #LatinChain #PiApps #PiNetwork \n\nLatinChain referral link:\n" + link_referrer + "\nReferral username:\n" + username_referrer);
           }
         }
     }
