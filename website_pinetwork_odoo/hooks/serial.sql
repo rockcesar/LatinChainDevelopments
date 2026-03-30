8552743271c283b9f84e90bfb2f65d8329bd6bdb
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-03-30 12:15:35 -0400

    Upgrade

--- website_pinetwork_odoo/hooks/serial.sql
+++ website_pinetwork_odoo/hooks/serial.sql
@@ -5,6 +5,11 @@ alter sequence pi_transactions_id_seq as bigint;
 alter sequence admin_apps_id_seq as bigint;
 alter sequence pi_users_id_seq as bigint;
 
+alter table pi_transactions alter app_id type bigint;
+alter table pi_transactions alter pi_user type bigint;
+alter table admin_apps alter pi_main_user type bigint;
+alter table pi_users alter pi_user_referrer_id type bigint;
+
 /*
 SELECT setval('pi_transactions_id_seq', (SELECT max(id) FROM pi_transactions));
 UPDATE pi_transactions SET id=DEFAULT;
