f2a32fcf63bd73aa7b2bc85f5afb59de6d5197d5
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-04-01 14:57:40 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -55,15 +55,23 @@ function openInNewTab(url) {
   
   // 2. Set the necessary attributes
   anchor.href = url;
-  anchor.target = '_self';
+  anchor.target = '_blank';
   //anchor.rel = 'noopener noreferrer';
-  anchor.rel = 'nofollow noopener';
+  anchor.rel = 'noopener noreferrer';
 
   // 3. Append to body (required for some browsers like Firefox to trigger the click)
   document.body.appendChild(anchor);
 
   // 4. Trigger the click event
-  anchor.click();
+  //anchor.click();
+  
+  const clickEvent = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true
+  });
+
+  anchor.dispatchEvent(clickEvent);
 
   // 5. Remove the element from the DOM immediately
   document.body.removeChild(anchor);
