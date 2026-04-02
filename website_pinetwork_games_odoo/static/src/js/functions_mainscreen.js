56618562488a31620f072357996698a67a7a25b1
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-04-02 06:41:35 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -50,6 +50,7 @@ function showImageModal(imageUrl, captionText) {
 }
 
 function openInNewTab(url) {
+  /*
   // 1. Create the anchor element
   const anchor = document.createElement('a');
   
@@ -66,7 +67,9 @@ function openInNewTab(url) {
   anchor.click();
 
   // 5. Remove the element from the DOM immediately
-  document.body.removeChild(anchor);
+  document.body.removeChild(anchor);*/
+  
+  window.location.assign(url);
 }
 
 async function colorboxLoaded()
