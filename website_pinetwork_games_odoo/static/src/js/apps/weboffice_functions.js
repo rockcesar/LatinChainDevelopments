b86c67323356f3f250105584aa695e0019ba87e3
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-05-06 10:47:20 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/apps/weboffice_functions.js
+++ website_pinetwork_games_odoo/static/src/js/apps/weboffice_functions.js
@@ -403,14 +403,14 @@ $(document).ready(function() {
         }
 
         // Disparar descarga estándar (Word y Excel)
-        let blob = new Blob(['\ufeff', content], { type: mimeType });
-        let url = URL.createObjectURL(blob);
+        // Usamos Data URI en base64 en lugar de Blob para mayor compatibilidad (ej. Pi Browser)
+        let base64Data = window.btoa(unescape(encodeURIComponent('\ufeff' + content)));
+        let dataUri = 'data:' + mimeType + ';base64,' + base64Data;
         let a = document.createElement('a');
-        a.href = url;
+        a.href = dataUri;
         a.download = filename;
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
-        URL.revokeObjectURL(url);
     });
 });
