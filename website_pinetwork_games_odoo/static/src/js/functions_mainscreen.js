7826d32aec8b03019f521853056f293db7fdf408
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-05-09 10:39:23 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -893,10 +893,10 @@ function copyToClipboard(text) {
 
 $( document ).ready(function() {
     
-    const video_latinchain = document.getElementById('loading-message-video');
+    /*const video_latinchain = document.getElementById('loading-message-video');
     video_latinchain.style.display="block";
     video_latinchain.width=250;
-    video_latinchain.height=150;
+    video_latinchain.height=150;*/
     
     btnvalue = $("#button_reward_ad").html();
     
--- website_pinetwork_odoo/static/src/js/common_functions.js
+++ website_pinetwork_odoo/static/src/js/common_functions.js
@@ -569,6 +569,37 @@ var googleTranslateElementInit = () => {
     }
 };
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    try{
+
+            const loading_latinchain = document.getElementById('loading-message');
+            
+            const loading_latinchain_pre = loading_latinchain.innerHTML;
+            
+            loading_latinchain.innerHTML = '<div style="display: block; ">' +
+                                                '<div style="display: block; ">' +
+                                                    loading_latinchain_pre +
+                                                '</div>' +
+                                                '<div class="justify-content-center text-center" style="display: block; margin-top: 15px;">' +
+                                                    '<video style="display: none; border-radius: 20%;" id="loading-message-video" autoplay="autoplay" muted="muted" playsinline="playsinline" loop="loop" width="0" height="0">' +
+                                                      '<source src="/website_pinetwork_games_odoo/static/src/video/video-presentation-latinchain.mp4?v=1.101" type="video/mp4" />' +
+                                                    '</video>' +
+                                                '</div>' +
+                                            '</div>';
+            
+            const video_latinchain = document.getElementById('loading-message-video');
+            video_latinchain.style.display="block";
+            video_latinchain.width=250;
+            video_latinchain.height=150;
+
+    }catch(e)
+    {
+    }
+        
+});
+
+
 /*
  * Here finish the language translation
  * */
