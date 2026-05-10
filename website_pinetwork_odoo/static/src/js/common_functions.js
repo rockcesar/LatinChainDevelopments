cae309c127a6da1291d4da06e231b655a53715f2
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-05-10 09:47:37 -0400

    Upgrade

--- website_pinetwork_odoo/static/src/js/common_functions.js
+++ website_pinetwork_odoo/static/src/js/common_functions.js
@@ -604,7 +604,7 @@ document.addEventListener("DOMContentLoaded", () => {
                                                 loading_latinchain_pre +
                                             '</div>' +
                                             '<div id="loading-message-section" class="justify-content-center text-center" style="display: none; margin-top: 15px;">' +
-                                                '<video style="display: none; border-radius: 30%; max-width: 250px; max-height: 150px; width: 250px; height: 150px; aspect-ratio: 16 / 9; object-fit: cover;" id="loading-message-video" autoplay="autoplay" muted="muted" playsinline="playsinline" loop="loop" width="250" height="150">' +
+                                                '<video style="display: none; border-radius: 30%; max-width: 250px; max-height: 150px; width: 250px; height: 150px; aspect-ratio: 16 / 9; object-fit: cover;" id="loading-message-video" muted="muted" playsinline="playsinline" loop="loop" width="250" height="150">' +
                                                   '<source src="/website_pinetwork_games_odoo/static/src/video/video-presentation-latinchain.mp4?v=1.102" type="video/mp4" />' +
                                                 '</video>' +
                                             '</div>' +
@@ -616,12 +616,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 
                 const video_latinchain = document.getElementById('loading-message-video');
                 const video_latinchain_section = document.getElementById('loading-message-section');
-                //video_latinchain.addEventListener('play', () => {
-                video_latinchain.style.display="block";
-                //video_latinchain.width=250;
-                //video_latinchain.height=150;
-                video_latinchain_section.style.display="block";
-                //});
+                video_latinchain.addEventListener('playing', () => {
+                    video_latinchain.style.display="block";
+                    //video_latinchain.width=250;
+                    //video_latinchain.height=150;
+                    video_latinchain_section.style.display="block";
+                });
+                
+                video_latinchain.play();
                 
             });
         });        
