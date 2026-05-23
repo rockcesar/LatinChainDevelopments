0ff959b47ee5ebb68a7378ccbf007b2579ad13b3
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-05-23 09:50:10 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -139,14 +139,14 @@ function colorboxLoadedMainnet()
             $('.darkmode-toggle--white').css('display', 'none');
     }else if(["Testnet ON", "Testnet OFF"].includes($("#mainnet").val()) && $("#nopopup").val() == false)
     {
-        openInNewTabAds(URLTestnet);
+        //openInNewTabAds(URLTestnet);
         //window.location.href = "https://ecosystem.latin-chain.com/page-1?v=1.115"+hashLatinChainGoogleTranslate;
         
-        $('.darkmode-toggle').css('display', 'none');
+        /*$('.darkmode-toggle').css('display', 'none');
         if($('.darkmode-toggle--white').length === 0)
             $('.darkmode-layer--button').css('display', 'none');
         else
-            $('.darkmode-toggle--white').css('display', 'none');
+            $('.darkmode-toggle--white').css('display', 'none');*/
     }else
     {
         $('.darkmode-toggle').css('display', 'block');
