2081fd34b681eaf0a21480c775064601ba1a5af1
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-06-03 07:19:13 -0400

    Upgrade

--- website_pinetwork_odoo/models/admin_apps.py
+++ website_pinetwork_odoo/models/admin_apps.py
@@ -362,7 +362,7 @@ class admin_apps(models.Model):
     """
     
     def _get_latinchain_specs(self):
-        return '/website_pinetwork_games_odoo/static/src/img/specs/latinchain-specs.jpg?v=1.104';
+        return '/website_pinetwork_games_odoo/static/src/img/specs/latinchain-specs.jpg?v=1.105';
     
     def _compute_points_latin_daily_total(self):
         for i in self:
