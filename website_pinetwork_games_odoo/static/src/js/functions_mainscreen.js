e76642048e47dabe1b110c44d89bbb6952dbd220
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-06-22 12:33:39 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -69,12 +69,12 @@ function openInNewTabAds(url) {
 
 function colorboxLoaded()
 {
-    var URLTestnet = "https://ecosystem.latin-chain.com/page-1/?v=1.127"+hashLatinChainGoogleTranslate;
+    var URLTestnet = "https://ecosystem.latin-chain.com/page-1/?v=1.128"+hashLatinChainGoogleTranslate;
     
     if(["Testnet ON", "Testnet OFF"].includes($("#mainnet").val()) && $("#nopopup").val() == false)
     {
         openInNewTabAds(URLTestnet);
-        //window.location.href = "https://ecosystem.latin-chain.com/page-1?v=1.115"+hashLatinChainGoogleTranslate;
+        //window.location.href = "https://ecosystem.latin-chain.com/page-1?v=1.128"+hashLatinChainGoogleTranslate;
         
         $('.darkmode-toggle').css('display', 'none');
         if($('.darkmode-toggle--white').length === 0)
@@ -95,7 +95,7 @@ function colorboxLoaded()
     {
         if($.colorbox && !colorbox_opened)
         {
-            $.colorbox({fixed: true, href:"https://ecosystem.latin-chain.com/page-1?v=1.112"+hashLatinChainGoogleTranslate, closeButton:false, overlayClose:false, escKey:false, iframe:true, width: "100%", height: "100%", maxWidth: "100%", maxHeight: "100%"});
+            $.colorbox({fixed: true, href:"https://ecosystem.latin-chain.com/page-1?v=1.128"+hashLatinChainGoogleTranslate, closeButton:false, overlayClose:false, escKey:false, iframe:true, width: "100%", height: "100%", maxWidth: "100%", maxHeight: "100%"});
             colorbox_opened = true;
             return false;
         }else{
@@ -117,12 +117,12 @@ function colorboxLoaded()
 
 function colorboxLoadedMainnet()
 {
-    var URLMainnet = "https://ecosystem.latin-chain.com/page-mainnet/?v=1.127"+hashLatinChainGoogleTranslate;
-    var URLTestnet = "https://ecosystem.latin-chain.com/page-1/?v=1.127"+hashLatinChainGoogleTranslate;
+    var URLMainnet = "https://ecosystem.latin-chain.com/page-mainnet/?v=1.128"+hashLatinChainGoogleTranslate;
+    var URLTestnet = "https://ecosystem.latin-chain.com/page-1/?v=1.128"+hashLatinChainGoogleTranslate;
     if(["Mainnet ON", "Mainnet OFF"].includes($("#mainnet").val()) && $("#nopopup").val() == false)
     {
         openInNewTabAds(URLMainnet);
-        //window.location.href = "https://ecosystem.latin-chain.com/page-mainnet?v=1.115"+hashLatinChainGoogleTranslate;
+        //window.location.href = "https://ecosystem.latin-chain.com/page-mainnet?v=1.128"+hashLatinChainGoogleTranslate;
         
         $('.darkmode-toggle').css('display', 'none');
         if($('.darkmode-toggle--white').length === 0)
@@ -152,7 +152,7 @@ function colorboxLoadedMainnet()
     {
         if($.colorbox && !colorbox_opened_mainnet)
         {
-            $.colorbox({fixed: true, href:"https://ecosystem.latin-chain.com/page-mainnet?v=1.108"+hashLatinChainGoogleTranslate, closeButton:false, overlayClose:false, escKey:false, iframe:true, width: "100%", height: "100%", maxWidth: "100%", maxHeight: "100%"});
+            $.colorbox({fixed: true, href:"https://ecosystem.latin-chain.com/page-mainnet?v=1.128"+hashLatinChainGoogleTranslate, closeButton:false, overlayClose:false, escKey:false, iframe:true, width: "100%", height: "100%", maxWidth: "100%", maxHeight: "100%"});
             colorbox_opened_mainnet = true;
             return false;
         }else{
