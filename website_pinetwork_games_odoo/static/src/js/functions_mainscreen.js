a1e6799997a387dc1c56735676b274cb97eb252b
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-07-27 09:46:16 -0400

    Upgrade

--- website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
+++ website_pinetwork_games_odoo/static/src/js/functions_mainscreen.js
@@ -32,7 +32,7 @@ var colorbox_opened_mainnet = false;
 var is_open_tab = false;
 var is_open_tab_ads = false;
 
-var eco_page_version = "?v=1.141";
+var eco_page_version = "?v=1.142";
 
 function setConfirmUnload(on) {
     unloadMessage(on);
